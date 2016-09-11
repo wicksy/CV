cb4ce88737a00b0affb5b00b3ba7a9269d1d0fdf
Martin Wicks <wicksy@wicksy.com>
2016-09-11 06:04:06 +0100

    Services test

---
+++ test/test_services.py
@@ -0,0 +1,21 @@
+import pytest
+
+@pytest.mark.parametrize("name, enabled, running", [
+  ("docker", "enabled", "running"),
+])
+
+def test_services(Service, name, enabled, running):
+  is_enabled = Service(name).is_enabled
+  print(is_enabled)
+  if enabled == "enabled":
+    assert is_enabled
+  else:
+    assert not is_enabled
+
+  is_running = Service(name).is_running
+  print(is_running)
+  if running == "running":
+    assert is_running
+  else:
+    assert not is_running
+
