72e0f5aa76740ff0239c089ec2deda299ad201f4
Martin Wicks <wicksy@wicksy.com>
2016-10-09 13:05:07 +0100

    Apply states in build

--- travis/build.sh
+++ travis/build.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -x
+#
+# Apply Salt States
 
+# Create Salt Links for States and Pillar
+#
 mkdir -p /srv
-
 ln -sf "$(pwd)/salt/roots" /srv/salt
 ln -sf "$(pwd)/salt/pillar" /srv/pillar
 
-ls -l
+# Apply States
+#
+salt-call --local -l debug state.apply
 
-#salt-call --local -l debug state.apply
+docker ps
+sleep 30
+curl "http://localhost:8080"
 
-#docker ps
-#sleep 30
-#curl "http://localhost:8080"
-
-#salt-call --version
+salt-call --version
 
 exit 0
