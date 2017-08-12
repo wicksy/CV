777201d7dac962e684eb95e6eae07f3905218e8f
Martin Wicks <wicksy@wicksy.com>
2017-08-12 14:27:51 +0100

    Add script to build and run image as salt docker states fail with not imported

---
+++ salt/roots/docker/files/docker-build-and-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Build image and run container specifically for Ubuntu Trusty where the docker salt states do
+# not seem to work properly (work ok on Xenial). Complain that docker module has not been loaded
+# even with multiple explicit reload_modules: true run beforehand.
+#
+
+cd /tmp/docker-lab/base && docker build -t wicksy/base:latest .
+cd /tmp/docker-lab/wicksycv && docker build -t wicksy/wicksycv:latest .
+
+docker run --detach --name wicksycv --publish 8080:8080 --volume /tmp/CV/:/data/cv/ wicksy/wicksycv:latest bash -c "cd /data/cv/mkdocs && /usr/bin/mkdocs serve --dev-addr 0.0.0.0:8080 --theme material"
+
+exit
