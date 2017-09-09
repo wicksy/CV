ff16d5108b060d50dacbc7d82febdb7459c8487b
Martin Wicks <wicksy@wicksy.com>
2017-09-09 18:32:42 +0100

    No longer need to build base container image

--- salt/roots/docker/files/docker-build-and-run.sh
+++ salt/roots/docker/files/docker-build-and-run.sh
@@ -4,10 +4,6 @@
 # not seem to work properly (work ok on Xenial). Complain that docker module has not been loaded
 # even with multiple explicit reload_modules: true run beforehand.
 #
-
-cd /tmp/docker-lab/base && docker build -t wicksy/base:latest .
 cd /tmp/docker-lab/wicksycv && docker build -t wicksy/wicksycv:latest .
-
 docker run --detach --name wicksycv --publish 8080:8080 --volume /tmp/CV/:/data/cv/ wicksy/wicksycv:latest bash -c "cd /data/cv/mkdocs && /usr/bin/mkdocs serve --dev-addr 0.0.0.0:8080 --theme material"
-
 exit
