ca260c908b4dd3f4fb2d108c7b566c9046f62fe6
Martin Wicks <wicksy@wicksy.com>
2017-10-05 06:06:20 +0100

    Add /srv link for ansible

--- travis/build.sh
+++ travis/build.sh
@@ -10,10 +10,12 @@ set -u
 set -o pipefail
 
 # Create Salt Links for States and Pillar
+# and link for Ansible
 #
 mkdir -p /srv
 ln -sf "$(pwd)/salt/roots" /srv/salt
 ln -sf "$(pwd)/salt/pillar" /srv/pillar
+ln -sf "$(pwd)/ansible" /srv/ansible
 
 # Apply States
 #
